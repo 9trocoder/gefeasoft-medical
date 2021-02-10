9ecf59e33632541978f7dd020dcb7bdf6b08fd20
yemicodes <yemicodes@gmail.com>
2021-02-10 04:58:56 +0300

    updated action index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
+
+class Root extends React.Component{
+  render() {
+    return(
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
